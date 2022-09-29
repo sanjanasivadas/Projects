7544f930fbb59afa1991547d59ba505fb0b50b13
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-09-29 13:45:46 +0530

    commit

---
+++ bin/for.dart
@@ -0,0 +1,11 @@
+//syntax => for(intialization; condition check; increment/decrement)
+void main() {
+  for(int i = 1 ; i <= 10 ; i++){
+    print(i);
+  }
+
+
+  for(int k = 10 ; k >= 1 ; k--){
+    print(k);
+  }
+}
