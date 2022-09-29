aed838cc6f7f4e2594407b5a754a68b6d965619b
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-09-29 13:21:49 +0530

    commit

---
+++ bin/switch.dart
@@ -0,0 +1,34 @@
+void main() {
+ var mark = 90;
+
+ switch (mark){
+  case 40 : print("just pass");
+  break;
+  case 50 : print("grade is d");
+  break;
+  case 60 : print("grade is c");
+  break;
+  case 70 : print("grade is b");
+  break;
+  case 80 : print("grade is a");
+  break;
+  default : print("none of these");
+ }
+
+
+var marks = "40";
+
+ switch (marks){
+  case "40" : print("just pass");
+  break;
+  case "50" : print("grade is d");
+  break;
+  case "60" : print("grade is c");
+  break;
+  case "70" : print("grade is b");
+  break;
+  case "80" : print("grade is a");
+  break;
+  default : print("none of these");
+ }
+}
