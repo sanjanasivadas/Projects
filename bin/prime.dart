90d069da7331586f1433b5cb09e4653b6439c67f
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-08 22:12:47 +0530

    commit

--- bin/prime.dart
+++ bin/prime.dart
@@ -1,16 +1,25 @@
 import 'dart:io';
-
 void main(){
+ int temp = 0;
  print('enter a number');
  int num = int.parse(stdin.readLineSync()!);
- int result=0;
- for(int i=2;i<=n~/i;i++)
- if(n % 1 == 0){
+ for(int i = 2; i <= num~/i; i++){
+ if(num % 1 == 0){
     temp = 1;
     break;
-  } }
-if(temp == 0){
+   } 
+  }
+  if(temp == 0){
   print('prime number');
-}else{
+  }else{
   print('not prime number');
+  }
 }
+
+//13 for i = 2 i <= 13~/2
+//             2 <= 6  true if (13 % 2 == 0) false temp = 0 i++
+//       i = 3 3 <= 13~/3
+//             3 <= 4  true if (13 % 3 == 0) false temp = 0 i++
+//       i = 4 4 <= 13~/4
+//             4 <= 3 false for loop exit
+//      if (temp == 0) => print prime number
