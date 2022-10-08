6df21218690beccca8b0549dfe6fc67fcd24399a
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-08 22:12:56 +0530

    commit

--- bin/prime.dart
+++ bin/prime.dart
@@ -3,13 +3,12 @@ void main(){
  int temp = 0;
  print('enter a number');
  int num = int.parse(stdin.readLineSync()!);
- for(int i = 2; i <= num~/i; i++){
- if(num % 1 == 0){
+ for(int i = 2 ; i <= num~/i ; i++){
+   if(num % i == 0){
     temp = 1;
     break;
-   } 
-  }
-  if(temp == 0){
+   } }
+if(temp == 0){
   print('prime number');
   }else{
   print('not prime number');
