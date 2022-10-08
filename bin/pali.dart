1aee5cc553788404459568097d63276037f60824
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-08 22:22:22 +0530

    commit

--- bin/pali.dart
+++ bin/pali.dart
@@ -16,3 +16,9 @@ if(rev == temp){
   print('not palindrome');
 }
 }
+//if 121    temp = 121
+/// 121 > 0  true  rem = 121 % 10 = 1 rev =0*10+1 = 1  num = 121~/10 = 12
+/// 12  > 0  true  rem = 12  % 10 = 2 rev =1*10+2 = 12 num = 12~/10  = 1
+/// 1   > 0  true  rem =  1  % 10 = 1 rev =12*10+1= 121 num = 1~/10  = 0
+/// 0   > 0  false exit from while
+/// rev == temp  121 == 121
