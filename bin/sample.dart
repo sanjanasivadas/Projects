8eddf09231c8331ac13d539d6ede4a928b89d9d9
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-09 11:34:14 +0530

    commit

--- bin/sample.dart
+++ bin/sample.dart
@@ -1,3 +1,17 @@
+import 'dart:io';
 void main() {
-print('welcome to dart'); 
+  //variable declaration
+  //datatype variablename = value;
+
+String name = "Sanjana";
+int age = 21;
+//string interpolation
+print("My name is ${name}"); //curly bracket to print more than one value
+print("I am ${age}yrs old");
+print("My name is ${name},I am ${age}yrs old");
+//stdout.write("My name is ${name}");
+//stdout.write("I am ${age}yrs old");
+
+int a = 70, b = 50;
+print('sum = ${a+b}');
 }
