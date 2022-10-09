4c8b2095ee012d2a5499c8fc9725bfbda926f625
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-09 11:39:13 +0530

    commit

--- bin/sample.dart
+++ bin/sample.dart
@@ -8,10 +8,10 @@ int age = 21;
 //string interpolation
 print("My name is ${name}"); //curly bracket to print more than one value
 print("I am ${age}yrs old");
-print("My name is ${name},I am ${age}yrs old");
-//stdout.write("My name is ${name}");
-//stdout.write("I am ${age}yrs old");
+//print("My name is ${name},I am ${age}yrs old");
+stdout.write("My name is ${name} ");
+stdout.write("I am ${age}yrs old ");
 
 int a = 70, b = 50;
-print('sum = ${a+b}');
+print('\nsum = ${a+b}');
 }
