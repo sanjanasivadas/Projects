c43c069f66a82ffe0c320c2173309a64f9333b74
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-10 12:34:46 +0530

    commit

--- bin/list.dart
+++ bin/list.dart
@@ -1,26 +1,28 @@
 void main() {
-  String name = "Hari";
+  //1. list with direct values - growable list
+  List mylist = [1,2,3,4,5,6,7];  //dynamic list as type is not mentioned
+  mylist.add('hello');
+  print(mylist);
+
+///list with direct values - growable list
+ // List<int> mylist = [1,2,3,4,5,6,7];  //dynamic list as type is not mentioned
+  //mylist.add('hello'); //hello string cannot be added to integer list
+  //print(mylist);
 
 
-///1. list using direct value
-       //index  0 
+  //2. List.empty() by default is not growable creates empty list
+  //var list1 = List.empty();  //if we want to add var list1 = List.empty(growable: true);
+  var list1 = List.empty(growable: true);
+  list1.add('hello');
+  list1.add('welcome');
+  print(list1);
 
 
-
-
-///2.List.empty()
-List list1 = List.empty(growable: true);
-list1.add(20);
-print(list1);
-
-///3. List.filled()
-List list2 = List.filled(5,2,growable: true);
-list2.add(2);
-list2[1] = 3;
-list2[2] = 5;
-list2[3] = 7;
-list2.add(10);
-print('index of 7 = ${list2.indexOf(7)}');
+///3. List.filled()  by default not growable
+var list2 = List.filled(5,1,growable: true); //list length 5 index means position 0 1 2 3 4
+list2[1] = 2;          //if we want to change the current value
+list2[4] = 6;
+list2.add(7);          //for adding new values
 print(list2);
 
 }
