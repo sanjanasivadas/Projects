bd7edea1d8d35df9e1e8771daab505b005e20422
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-10 15:52:16 +0530

    commit

---
+++ bin/thiskey.dart
@@ -0,0 +1,13 @@
+class ThisDemo {
+  String? name;
+  int? age;
+  ThisDemo(this.name , this.age);
+  void show(){
+    print(name);
+    print(age);
+  }
+}
+void main() {
+  ThisDemo oj = ThisDemo("Anna", 22);
+  oj.show();
+}
