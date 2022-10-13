443928bb963c78a53539d02a2e2f151a29bf7f85
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-13 15:50:51 +0530

    commit

---
+++ bin/mixins.dart
@@ -0,0 +1,26 @@
+mixin A{
+  int a = 10;
+  void show(){
+    print('inside show');
+  }
+  //void disp();
+}
+
+mixin B{
+  int b = 200;
+  void view(){
+    print('inside view');
+  }
+}
+class C with A,B{
+  void classc(){
+    print('class c function');
+  }
+}
+void main() {
+  C obj = C();
+  obj.show();
+  print(obj.a+obj.b);
+  obj.view();
+  obj.classc();
+}
