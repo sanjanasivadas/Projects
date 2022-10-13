7b13a8a72989a33ae944387734f2f3b74227e201
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-13 16:05:23 +0530

    commit

---
+++ bin/myself.dart
@@ -0,0 +1,16 @@
+class Myself{
+  void mydetails(String name,int age,String fname,String mname,String designation){
+    print('Name                  : $name');
+    print('Age                   : $age');
+    print('Fathers name          : $fname');
+    print('Mothers name          : $mname');
+    print('Designation           : $designation');
+  }
+}
+class Degree{
+  void degdetails(String clg,String course,int duration){
+    print('Clg                   : $clg');
+    print('Course                : $name');
+
+  }
+}
