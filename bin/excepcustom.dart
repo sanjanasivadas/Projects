b353d0d781860027e0bea470cea7814607c471f8
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-15 14:26:59 +0530

    commit

--- bin/excepcustom.dart
+++ bin/excepcustom.dart
@@ -1,12 +1,18 @@
-class MyException implements Exception{
+//this is user defined exception class
+  //if a class created has to work as exception class. we have to implement the buildin exception class
+  //we use constructor to pass our custom msg
+
+class MyException implements Exception{    //to pass our own custom messages
   String msg;
-  MyException(this.msg)
-  
-  @override
-  String toString(){
+  MyException(this.msg);
+                            //when we type toString the @override will automatically come
+   @override
+  String toString() {
+   
     return msg;
   }
-}
+} 
+
 void mark(int mark){
   if(mark < 40){
     throw MyException("Mark is less than 40");
@@ -16,7 +22,7 @@ void mark(int mark){
 }
 void main() {
   try {
-    mark(4);
+    mark(39);
     }catch(e){
       print("exception occured $e");
     }
--- bin/typedef.dart
+++ bin/typedef.dart
@@ -1,20 +1,33 @@
-typedef myfunction(int num1, int num2);
+typedef myfunction(int num1, int num2);  //aliasing function(replica of function)=> type def
+//if there is more than 1 function and the no and type of parameter is same. we can access it using a common name
 
 add(int a, int b){
   print("sum = ${a+b}");
-  add(5,6);
+  //add(5,6);
 }
 sub(int a,int b){
   print("sub = ${a-b}");
 }
-mul(int a, int b,myfunction m){
-  print("mul = ${a*b}");
+mul(int a, int b,myfunction m){           //mul(int a, int b,int c){
+  print("mul = ${a*b}");                  // print("mul = ${a*b*c}");
   m(a,b);
 }
 show(String n,int a){
-  print("my name is $n, i am $a yrs old");
+  print("my name is $n, I am $a yrs old");
 }
 void main() {
-  mul(3, 4, add);
-  mul(5, 8, sub);
+  mul(3,4,add);
+  mul(5,8,sub);
+
+  //myfunction m = add;
+  //m(8,9);
+  
+  //myfunction meth;
+  //meth = add;
+  //mul(2, 5, (num1, num2) => num1+num2);
+  // add(1, 2);
+  // sub(1, 2);
+          ///mul(4, 5, 6);
+  //show('Sanjana', 21);
+ 
 }
