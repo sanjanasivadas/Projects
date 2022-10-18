363e9674d093da7d35db90b71d1cf0f766572fc2
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-18 12:33:20 +0530

    commit

--- bin/thiskey.dart
+++ bin/thiskey.dart
@@ -2,12 +2,13 @@ class ThisDemo {
   String? name;
   int? age;
   ThisDemo(this.name , this.age);
+
   void show(){
     print(name);
     print(age);
   }
 }
 void main() {
-  ThisDemo oj = ThisDemo("Anna", 22);
-  oj.show();
+  ThisDemo obj = ThisDemo("Anna", 22);
+  obj.show();
 }
--- bin/thiskey2.dart
+++ bin/thiskey2.dart
@@ -1,13 +1,17 @@
 class ThisDemo {
   String? name;
   int? age;
-  ThisDemo( String name , int age);
-  void show(){
-    this.n = name;
-    this.a = age;
+
+  ThisDemo( String name , int age){
+    this.name = name;
+    this.age = age;
   }
+   void show(){
+    print(name);
+    print(age);
+   }
 }
 void main() {
-  ThisDemo oj = ThisDemo("Anna", 22);
-  oj.show();
+  ThisDemo obj = ThisDemo("Anna", 22);
+  obj.show();
 }
