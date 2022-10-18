961edb18d432587908f6b3e3eb77dbee4d0c6df2
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-18 15:41:58 +0530

    commit

--- bin/test5.dart
+++ bin/test5.dart
@@ -2,8 +2,11 @@ void main() {
   List<int> a = [1,10,2,3,5,8,28,41,34,5,62];
   print('a = $a');
   for(int index = 0; index < 11 ; index++){
-    if(a[index] > 2){
-      print("${a[index]} is greater than 2");
-    }
+    if(a[index] > 2  && a[index]%4 == 0){
+      print("${a[index]} is greater than 2 and muliple of 4");
+     }
+    //if(a[index]%4 == 0){
+    // print("${a[index]} is multiple of 4");
+    //}
   }
 }
