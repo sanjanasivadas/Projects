da801b88dba4446af3091f2290c099a9864f7ec5
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-18 09:57:14 +0530

    commit

--- bin/assi.dart
+++ bin/assi.dart
@@ -3,6 +3,7 @@ class Student{
   String name = "Sanjana";
   late int phn;
   late int age;
+  final dob = '12/08/2001';
   static String school = "Amrita vidyalayam";
    
    void showdetails(String details){
@@ -13,6 +14,7 @@ class Student{
   void main() {
   Student obj = Student();
   print("Name              : ${obj.name}");
+  print("Dob               : ${obj.dob}");
   print("School            : ${Student.school}");
   obj.showdetails("SANJANA,PULIKOTE ROAD,THALASSERY");
   //Student sanjana = Student();                                  
