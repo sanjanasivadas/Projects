1abec6db559c8f1ee7d5df9fad116a4d5593f6d4
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-10-26 11:06:50 +0530

    commit

--- bin/flutter_application_1/lib/main.dart
+++ bin/flutter_application_1/lib/main.dart
@@ -10,19 +10,31 @@ class Splash extends StatelessWidget{
   @override
   Widget build(BuildContext context){
        return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const[
-              Icon(
-              Icons.account_circle,
-              color: Colors.pink,
-              size: 90,
-              ),
-              Text("MYAPP",style: TextStyle(fontSize: 40,color: Colors.green)),
-            ],
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage("https://mir-s3-cdn-cf.behance.net/project_modules/disp/496ecb14589707.562865d064f9e.png")
+            )
+             
           ),
-          )
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children:  [
+                Image.asset("assets/images/chat-bot.png",height: 100,width: 100,),           //fetching image from assets
+               //const Image(image: AssetImage("assets/images/chat-bot.png"),),
+               // Icon(
+                //Icons.account_circle,
+               // color: Colors.pink,
+                //size: 90,
+               // ),
+               const Text("MYAPP", style: TextStyle(fontSize: 40, color: Colors.green)),
+              ],
+            ),
+          ),
+        ),
        );
   }
   }
