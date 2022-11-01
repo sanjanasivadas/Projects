c4ea52a1b55c40e33c63cd40dd4f5359aec92d5f
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-11-01 14:27:23 +0530

    commit

--- bin/flutter_application_1/lib/bottom.dart
+++ bin/flutter_application_1/lib/bottom.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home:MyBottomnavbar()));
+  runApp(MaterialApp(
+  home:MyBottomnavbar()));
 }
 class MyBottomnavbar extends StatefulWidget{
   @override
@@ -10,43 +11,37 @@ class MyBottomnavbar extends StatefulWidget{
 
 class _MyBottomnavbarState extends State<MyBottomnavbar> {
 
-  var bodies =<Widget> [
-    //List_with_builder(),
-    Card(color:Colors.teal,child: Padding(padding: EdgeInsets.all(50.0),child: Text("Homepage"),),),
-    Card(color:Colors.red,child: Padding(padding: EdgeInsets.all(50.0),child: Text("Search"),),),
-    Card(color:Colors.blueAccent,child: Padding(padding: EdgeInsets.all(50.0),child: Text("Favourites"),),),
-    Card(color:Colors.purple,child: Padding(padding: EdgeInsets.all(50.0),child: Text("Profile"),),),
-  ];
-  int selectedindex = 0;
+ var bodies = [Card(color: Colors.teal,child: Padding(padding:EdgeInsets.all(50.0),child: Text("HomePage"),),)
+ ];
+ int selectedindex = 0;
+ void onitemtap(int index){
+  setState(() {
+    selectedindex = index;
+  });
+ }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("BottomBar"),
+   return Scaffold(
+    appBar: AppBar(
+      title: Text("Bottombar"),),
+    body: const Center(
+      child: 
       ),
-      body: const Center(
-        child: Card(
-          color: Colors.teal,
-          child: Padding(
-            padding: EdgeInsets.all(50.0),
-            child: Text("HomePage"),
-            ),
+      bottomNavigationBar: BottomNavigationBar(                                  //bottomnavbar should be given inside scaffold outside appbar and body
+       type: BottomNavigationBarType.fixed,
+       selectedItemColor: Colors.pink,
+       unselectedItemColor: Colors.yellow,
+       backgroundColor: Colors.black,
+       currentIndex: selectedindex,
+       onTap: onitemtap,
+        items:const [
+               BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home",backgroundColor: Colors.black),
+               BottomNavigationBarItem(icon: Icon(Icons.search),label: "Search",backgroundColor: Colors.teal),
+               BottomNavigationBarItem(icon: Icon(Icons.favorite),label: "Favourites",backgroundColor: Colors.lime),
+               BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Profile",backgroundColor: Colors.lightGreen)
+        ] 
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        selectedItemColor: Colors.pink,
-        unselectedItemColor: Colors.yellow,
-        backgroundColor: Colors.black,
-        currentIndex: selectedindex,
-        onTap: onitemtap,
-        items: const[
-          BottomNavigationBarItem(icon: Icon(Icons.home),label:"Home",backgroundColor: Colors.black,),
-          BottomNavigationBarItem(icon: Icon(Icons.search),label:"Search",backgroundColor: Colors.teal,),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite),label:"Favourite",backgroundColor: Colors.cyan,),
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle),label:"Account",backgroundColor: Colors.orange,),
-          
-        ]),
-    );
+   );
   }
 }
