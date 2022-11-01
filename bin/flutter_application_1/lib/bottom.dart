704a2334f2746375f6ce79679405a8751e2ce931
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-11-01 21:59:55 +0530

    commit

--- bin/flutter_application_1/lib/bottom.dart
+++ bin/flutter_application_1/lib/bottom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/loginassi.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,7 +12,12 @@ class MyBottomnavbar extends StatefulWidget{
 
 class _MyBottomnavbarState extends State<MyBottomnavbar> {
 
- var bodies = [Card(color: Colors.teal,child: Padding(padding:EdgeInsets.all(50.0),child: Text("HomePage"),),)
+ var bodies = <Widget> [
+  LoginPage(),
+  //Card(color: Colors.teal,child: Padding(padding:EdgeInsets.all(50.0),child: Text("HomePage"),),),
+  Card(color: Colors.red,child: Padding(padding:EdgeInsets.all(50.0),child: Text("Search Page"),),),
+  Card(color: Colors.blueAccent,child: Padding(padding:EdgeInsets.all(50.0),child: Text("Favourites Page"),),),
+  Card(color: Colors.purpleAccent,child: Padding(padding:EdgeInsets.all(50.0),child: Text("Account Page"),),)
  ];
  int selectedindex = 0;
  void onitemtap(int index){
@@ -25,19 +31,19 @@ class _MyBottomnavbarState extends State<MyBottomnavbar> {
    return Scaffold(
     appBar: AppBar(
       title: Text("Bottombar"),),
-    body: const Center(
-      child: 
+    body:  Center(
+      child: bodies.elementAt(selectedindex),
       ),
       bottomNavigationBar: BottomNavigationBar(                                  //bottomnavbar should be given inside scaffold outside appbar and body
-       type: BottomNavigationBarType.fixed,
+       type: BottomNavigationBarType.shifting,
        selectedItemColor: Colors.pink,
        unselectedItemColor: Colors.yellow,
        backgroundColor: Colors.black,
        currentIndex: selectedindex,
        onTap: onitemtap,
         items:const [
-               BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home",backgroundColor: Colors.black),
-               BottomNavigationBarItem(icon: Icon(Icons.search),label: "Search",backgroundColor: Colors.teal),
+               BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home",backgroundColor: Colors.teal),
+               BottomNavigationBarItem(icon: Icon(Icons.search),label: "Search",backgroundColor: Colors.orange),
                BottomNavigationBarItem(icon: Icon(Icons.favorite),label: "Favourites",backgroundColor: Colors.lime),
                BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Profile",backgroundColor: Colors.lightGreen)
         ] 
