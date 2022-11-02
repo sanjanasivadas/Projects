9822c8f2d9e05d9d271f711c50234666e663760a
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-11-02 14:03:48 +0530

    commit

--- bin/flutter_application_1/lib/grid.dart
+++ bin/flutter_application_1/lib/grid.dart
@@ -9,16 +9,23 @@ void main() {
 class Grid extends StatelessWidget{
   
   var images = [
-   
+   "https://images.squarespace-cdn.com/content/v1/5d9767b1eb25db6ebdc03a70/1588817684139-VD5DBT6V5MS6FI0LS62G/endgame.png",
+   "https://images.squarespace-cdn.com/content/v1/5d9767b1eb25db6ebdc03a70/1588817684139-VD5DBT6V5MS6FI0LS62G/endgame.png",
+   "https://images.squarespace-cdn.com/content/v1/5d9767b1eb25db6ebdc03a70/1588817684139-VD5DBT6V5MS6FI0LS62G/endgame.png",
+   "https://images.squarespace-cdn.com/content/v1/5d9767b1eb25db6ebdc03a70/1588817684139-VD5DBT6V5MS6FI0LS62G/endgame.png",
+   "https://images.squarespace-cdn.com/content/v1/5d9767b1eb25db6ebdc03a70/1588817684139-VD5DBT6V5MS6FI0LS62G/endgame.png",
+   "https://images.squarespace-cdn.com/content/v1/5d9767b1eb25db6ebdc03a70/1588817684139-VD5DBT6V5MS6FI0LS62G/endgame.png",
+   "https://images.squarespace-cdn.com/content/v1/5d9767b1eb25db6ebdc03a70/1588817684139-VD5DBT6V5MS6FI0LS62G/endgame.png",
+
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
      appBar: AppBar(
-      title: Text("My Grid"),
+      title: const Text("My Grid"),
       ),
       body: GridView.builder(
-       padding: EdgeInsets.all(20),
+       padding: const EdgeInsets.all(20),
 
         itemCount: images.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
