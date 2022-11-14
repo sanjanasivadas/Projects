fd2efa1ead57a71363fec62470c6520921964c0f
sanjanasivadas <sanjana.velikoth@gmail.com>
2022-11-14 19:43:43 +0530

    commit

---
+++ bin/flutter_application_1/lib/productgrid_dummy.dart
@@ -0,0 +1,30 @@
+const List<Map<String,dynamic>> dummyProducts = [
+  {
+    "id":1,
+    "name": "Apple",
+    "price": 110,
+    "description": "Fruit, Red colour",
+    "image": "https://images.unsplash.com/photo-1576179635662-9d1983e97e1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+   {
+    "id":2,
+    "name": "Mango",
+    "price": 90,
+    "description": "Fruit, Yellow colour",
+    "image": "https://images.unsplash.com/photo-1628695323145-5be0b8fde365?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=926&q=80"
+  },
+   {
+    "id":3,
+    "name":"Avocado",
+    "price":150,
+    "description":"Fruit, Green colour",
+    "image": "https://images.unsplash.com/photo-1583029901628-8039767c7ad0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+  },
+   {
+    "id":4,
+    "name":"Tomato",
+    "price":20,
+    "description":"Vegetable, Red colour",
+    "image": "https://images.unsplash.com/photo-1509963906410-fceef97f22f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+  },
+];
